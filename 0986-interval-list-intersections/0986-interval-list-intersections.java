b9db1bdd700e65641e06971312a8fc81515151d5
Aryan Mishra <aryanmish96@gmail.com>
2026-07-20 22:47:40 +0530

    Time: 4 ms (49.55%), Space: 47.6 MB (19.87%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,25 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
 
-        List<int[]> answer = new ArrayList<>();
+        int n = firstList.length;
+        int m = secondList.length;
+
+        int[][] result = new int[n + m][2];
 
         int i = 0;
         int j = 0;
+        int index = 0;
 
-        while (i < firstList.length && j < secondList.length) {
+        while (i < n && j < m) {
 
             int start = Math.max(firstList[i][0], secondList[j][0]);
             int end = Math.min(firstList[i][1], secondList[j][1]);
 
-            // Overlap exists
+            // Intersection exists
             if (start <= end) {
-                answer.add(new int[]{start, end});
+                result[index][0] = start;
+                result[index][1] = end;
+                index++;
             }
 
-            // Move the interval which finishes first
+            // Move the interval that ends first
             if (firstList[i][1] < secondList[j][1]) {
                 i++;
             } else {
@@ -27,6 +32,6 @@ class Solution {
             }
         }
 
-        return answer.toArray(new int[answer.size()][]);
+        return Arrays.copyOf(result, index);
     }
 }
